cb8779591d426368ada9de184c595b112a398c6d
Matheus <matheus.tavares45@gmail.com>
2021-01-01 15:06:06 -0300

    refactoring pokemon service

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,59 +1,67 @@
 import { Request, Response } from "express";
 import { MongooseDocument } from 'mongoose';
 import { Pokemon } from "../models/pokemon";
-import { WELCOME_MESSAGE } from '../constants/pokeApi';
 
 export class PokeService {
-    public welcomeMessage(req: Request, res: Response) {
-        return res.status(200).send(WELCOME_MESSAGE);
-    }
+  public getAllPokemon(req: Request, res: Response) {
+    Pokemon.find({}, async (error: Error, pokemon: MongooseDocument) => {
+      if(error) {
+          res.send(error);
+          return;
+      }
+      res.json(pokemon);
+    });
+  }
 
-    public getAllPokemon(req: Request, res: Response) {
-        Pokemon.find({}, async (error: Error, pokemon: MongooseDocument) => {
-            if(error) {
-                res.send(error);
-            }
-            res.json(pokemon);
-        });
-    }
+  public getPokemonById(req: Request, res: Response) {
+    const pokemonId = req.params.id;
+    Pokemon.findById(pokemonId, (error: Error, pokemon: MongooseDocument) => {
+      if (error) {
+        res.send(error);
+        return;
+      }
+      res.send(pokemon);
+    })
+  }
 
-    public addNewPokemon(req: Request, res: Response) {
-        const newPokemon = new Pokemon(req.body);
-        newPokemon.save(async (error: Error, pokemon: MongooseDocument) => {
-            if (error) {
-                res.send(error);
-            }
-            await pokemon.populate('type').execPopulate();
-            await pokemon.populate('subtype').execPopulate();
-            res.json(pokemon);
-        })
-    }
+  public addNewPokemon(req: Request, res: Response) {
+    const newPokemon = new Pokemon(req.body);
+    newPokemon.save(async (error: Error, pokemon: MongooseDocument) => {
+      if (error) {
+          res.send(error);
+          return;
+      }
+      res.json(pokemon);
+    })
+  }
 
-    public deletePokemon(req: Request, res: Response) {
-        const pokemonID = req.params.id;
-        Pokemon.findByIdAndDelete(pokemonID, (error: Error, deleted: any) => {
-            if (error) {
-                res.send(error);
-            }
-            const message = deleted ? 'Deleted successfully' : 'Pokemon not found';
-            res.send(message);
-        })
-    }
+  public deletePokemon(req: Request, res: Response) {
+    const pokemonId = req.params.id;
+    Pokemon.findByIdAndDelete(pokemonId, (error: Error, deleted: any) => {
+      if (error) {
+        res.send(error);
+        return;
+      }
+      const message = deleted ? 'Deleted successfully' : 'Pokemon not found';
+      res.send(message);
+    })
+  }
 
-    public updatePokemon(req: Request, res: Response) {
-        const pokemonID = req.params.id;
-        Pokemon.findByIdAndUpdate(
-            pokemonID,
-            req.body,
-            (error: Error, pokemon: any) => {
-                if (error) {
-                    res.send(error);
-                }
-                const message = pokemon 
-                    ? 'Updated successfully' 
-                    : 'Pokemon not found';
-                res.send(message);
-            }
-        );
-    }
+  public updatePokemon(req: Request, res: Response) {
+    const pokemonId = req.params.id;
+    Pokemon.findByIdAndUpdate(
+      pokemonId,
+      req.body,
+      (error: Error, pokemon: any) => {
+        if (error) {
+          res.send(error);
+          return;
+        }
+        const message = pokemon 
+          ? 'Updated successfully' 
+          : 'Pokemon not found';
+        res.send(message);
+      }
+    );
+  }
 }
