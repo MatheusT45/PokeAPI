72e0262d9c80d6873155aca9b36e74f2bc439285
Matheus <matheus.tavares45@gmail.com>
2021-01-01 15:06:34 -0300

    pokemon model changes

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
 
 const PokemonSchema = new mongoose.Schema({
-    name: String,
-    gender: String,
-    type: {
+    number:{ type: Number, required: true , unique: true },
+    name: { type: String, required: true },
+    hasGender: { type: Boolean, default: true },
+    description: String,
+    type: [{
         ref: 'PokeType',
         type: mongoose.Schema.Types.ObjectId,
-    },    
-    subtype: {
-        ref: 'PokeType',
-        type: mongoose.Schema.Types.ObjectId,
-    },
+    }],
     height: Number,
     weight: Number,
-    photo: String
+    sprite: String
 });
 
 export const Pokemon = mongoose.model("Pokemon", PokemonSchema);
