bad605b1bc9b2fdfb8605cf9764452da80232a72
Matheus <matheus.tavares45@gmail.com>
2021-01-05 13:01:07 -0300

    renaming database and changing constants

--- src/constants/pokeApi.ts
+++ src/constants/pokeApi.ts
@@ -1,3 +1,10 @@
 export const PORT = 9001;
-export const WELCOME_MESSAGE = "Welcome to pokeAPI!";
-export const MONGO_URL = "mongodb://localhost:27017/Pokemon";
+export const WELCOME_MESSAGE = 'Welcome to pokeAPI';
+
+export const MONGO_USERNAME = 'red';
+export const MONGO_PASSWORD = '';
+export const MONGO_HOSTNAME = 'localhost';
+export const MONGO_PORT = '27017';
+export const MONGO_DB = 'pokemon';
+
+export const MONGO_URL = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
